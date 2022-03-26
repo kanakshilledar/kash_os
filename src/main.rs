dac31727094a7c6f72f268cf868f8361ac18012d
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2022-03-26 15:55:50 +0530

    chapter-01-created entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// no rust standard library 
+#![no_std]
+// disable rust level entry points
+#![no_main]
+use core::panic::PanicInfo;
+
+// dont mangle the name of this function 
+//  mangleing is cryptographic has assigned to each function by rust
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // this function is entry point, liker looks for function
+    // named _start by default
+    loop {}
+}
+
+// function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
